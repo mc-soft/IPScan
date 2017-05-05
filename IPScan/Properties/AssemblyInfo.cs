bb5bda4e07746f19a2c68efa1e43f8a29583dabd
mc-soft <matt@m-croston.co.uk>
2017-05-05 16:06:06 +0100

    updated version info

--- IPScan/Properties/AssemblyInfo.cs
+++ IPScan/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IPScan")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("IPScan is a multi-threaded .NET library written in C# that was created to generate IP addresses using specified masks and ranges")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("http://www.m-croston.co.uk/")]
 [assembly: AssemblyProduct("IPScan")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
